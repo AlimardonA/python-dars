5015bdcbe2a57b4ddd451c248f32b1343fd71a79
AlimardonA <152531268+AlimardonA@users.noreply.github.com>
2024-08-12 19:02:37 +0500

    first commit

---
+++ 1-dars.py
@@ -0,0 +1,31 @@
+# ##### string metodlari #####
+# print("hello word")
+# print("hello \tword")
+# print("hello \nword")
+# ism = "James"
+# print(ism.upper()) # matni katta harfda chiqarish
+# print(ism.lower()) # matini kichik hariflarda chop etish
+# print(ism.title()) # matinni faqat bosh harflarini katta bilan chop etish
+# print (ism.capitalize()) # matini faqat birinchi harifini katta bilan chop etish!
+
+
+
+
+
+# kocha ="Bog'bon"
+# mahalla ="Sog'bon"
+# tuman ="Bodomzor"
+# viloyat = "Samarqand"
+# print (f"{kocha}, kochasi")
+
+# kocha =input("kochangizni kiriting\n >>>")
+# mahala =input("mahalangizni kiriting\n>>>")
+# tuman =input("tumaningizni kiritng\n>>>")
+# viloyat = input("viloyatingizni kiriting\n>>>")
+# print(f"assalomu alekum")
+
+# a = int(input("A sonni kiriting"))
+# b = a*12
+# print("{a} sonni 12 songa kopaytirsak {b} natijani olamiz".format(a=a,b=b))
+# print(f" {a} sonini 12 ga kopaytirganda natija {b}")
+
---
+++ 2-dars.py
@@ -0,0 +1,25 @@
+##### foydalnuvchi kiritgan sonni kvadirati va kubini hisoblash####
+# a = int(input("Istalgan son kiriting:\n >>>>"))
+# b = a**2
+# c = a**3
+# print(a, "ning kvadirati", b, "ga teng")
+# print(a, "ning kubi", c, "ga teng")
+
+
+##### Foydalanuvchidan yoshini surab tug'ilgan yilini aniqlash dasturini tuzish
+
+
+
+
+# a = int(input("Yoshingiz nechida?\n>>>"))
+# b = 2024-a
+# print("Siz", b, "da tug'ilgansiz")
+
+# a = int(input("Birinchi sonni kiriting:\n>>>"))
+# b = int(input("Ikkinchi sonni kiriting:\n>>>"))
+# print("{}+{}=".format(a,b),a+b)
+# print(f"{a}*{b}=", a*b)
+# print(f"{a}/{b}=", a/b)
+
+
+
