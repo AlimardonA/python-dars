131957b1bb56733ceb8b09b3bdaed2092ce16cc8
AlimardonA <152531268+AlimardonA@users.noreply.github.com>
2024-08-20 09:36:31 +0500

    uyga_vazifa_case

---
+++ uyga_vazifa_case.py
@@ -0,0 +1,36 @@
+######case1
+# x = float(input("x sonni kiriting: "))
+# match x:
+#     case 1:
+#         print("Dushanba")
+#     case 2:
+#         print("Seshanba")
+#     case 3:
+#         print("Chorshanba")
+#     case 4:
+#         print ("Payshnba")
+#     case 5:
+#         print ("Juma")
+#     case 6:
+#         print("Shanba")
+#     case 7:
+#         print("Yakshanba")
+#     case _:
+#         print("Notugri raqam")
+######case2
+# x = float(input("x sonni kiriting: "))
+# match x:
+#     case 1:
+#         print("yomon")
+#     case 2:
+#         print("qoniqarsiz")
+#     case 3:
+#         print("qoniqarli")
+#     case 4:
+#         print ("yaxshi")
+#     case 5:
+#         print ("Alo")
+#     case _:
+#         print("Notugri raqm kiritildi")
+
+######case3
