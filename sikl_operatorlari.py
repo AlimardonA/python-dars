070374382210615c844d11a6bfd1770791e55f09
AlimardonA <152531268+AlimardonA@users.noreply.github.com>
2024-08-21 14:00:57 +0500

    YANA

--- sikl_operatorlari.py
+++ sikl_operatorlari.py
@@ -1,6 +1,26 @@
-k = int(input("takrorlanayotgan kiriting:\n>>>"))
-n = int(input("nechi marta takrorlanishini kiriting:\n>>>"))
-for i in range(int(n)):
-    i=k
+##for1
+# k = int(input("takrorlanayotgan kiriting:\n>>>"))
+# n = int(input("nechi marta takrorlanishini kiriting:\n>>>"))
+# for i in range(n):
+#     i=k
+#     print(i)
+##for2
+# a = int(input("a soni kiriting:\n>>>"))
+# b = int(input("b soni kiriting:\n>>>"))
+# s =0
+# for i in range(a,b+1):
+#     s+=1
+#     print(i)
+# print(f"Chiqarilgam sonlar soni {s}")
+##for3
+a = int(input("a soni kiriting:\n>>>"))
+b = int(input("b soni kiriting:\n>>>"))
+s =0
+for i in range(b,a,-1):
+    s+=1
     print(i)
+print(f"Chiqarilgam sonlar soni {s}")
+
+
+
    
