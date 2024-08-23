a999b3257a3ff7e3027d2d304b978216229dded6
AlimardonA <152531268+AlimardonA@users.noreply.github.com>
2024-08-23 14:20:51 +0500

    sikl_while

---
+++ sikl_while_operatori.py
@@ -0,0 +1,40 @@
+##1-while
+# a = int(input("a sonini kiriting:"))
+# b = int(input("b sonini kiriting:"))
+# while a>=b:
+#     a-=b
+# print(a)
+##2-while
+# s = 0 
+# a = int(input("a sonini kiriting:"))
+# b = int(input("b sonini kiriting:"))
+# while a>=b:
+#     a-=b
+#     s+=1
+# print(s)
+##3-while
+# k=a = int(input("a sonini kiriting:"))
+# b = int(input("b sonini kiriting:"))
+# s = 0
+# while a>b:
+#     a-=b
+#     s+=1
+# print(f"{k} ni {b} ga bo'lganda {s} butun {a} qoldiq qoladi")
+##4-while
+# n = int(input("n sonini kiriting: "))
+# while n >= 1:
+#     if n % 3 == 0:
+#         print("uchga qoldiqsiz bo'linadi")
+#         break
+#     else:
+#         print("uchga qoldiqsiz bo'linmaydi")
+#         n = int(input("n sonini boshqatdan kiriting: "))  # Yangi son kiritishni so'raydi
+##5-while   
+# n = int(input("n sonini kiriting: ")) 
+# k=0 
+# while n>=2:
+#     n/=2
+#     k+=1
+# print(k)
+##6-while
+
