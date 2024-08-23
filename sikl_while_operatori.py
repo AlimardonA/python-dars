f8b486997fdeb8cc7fdf7c9a15bc7501f60ebcc3
AlimardonA <152531268+AlimardonA@users.noreply.github.com>
2024-08-23 17:38:16 +0500

    oxiri

--- sikl_while_operatori.py
+++ sikl_while_operatori.py
@@ -31,10 +31,24 @@
 #         n = int(input("n sonini boshqatdan kiriting: "))  # Yangi son kiritishni so'raydi
 ##5-while   
 # n = int(input("n sonini kiriting: ")) 
-# k=0 
-# while n>=2:
+# k=1 
+# while n/2!=1:
 #     n/=2
 #     k+=1
 # print(k)
 ##6-while
+# k=1
+# n = int(input("n sonini kiriting: ")) 
+# while n>=1:
+#     k*= n
+#     n -= 2
+# print(k)
+##7-while
+n = int(input("n sonini kiriting: "))
+k=1
+while k**2<=n:
+    k+=1
+print(k)
+    
+
 
