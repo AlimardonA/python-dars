fab5994cacf4b542bdf6d8a37f0740fdb16fdffd
AlimardonA <152531268+AlimardonA@users.noreply.github.com>
2024-08-26 17:32:36 +0500

    oxiri

--- 3-dars.py
+++ 3-dars.py
@@ -7,6 +7,21 @@
 # sonlar.append(1)
 # print(sonlar)
 
+a = [ ]
+s = 1
+k= 0
+while True:
+    mahsulot = input(f" {s}-Masulotni kiriting:\n >>>")
+    a.append(mahsulot)
+    javob = input("Yana mahsulot qushasizmi?(ha/yo'q)\n>>>")
+    if javob == "ha":
+        s+=1
+    else:
+        break
+for i in a:
+    k+=1
+    print(f"{k}.{i.title()}")
+
 
 
 
